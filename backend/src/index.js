e1bd63b9b6d65c3d54f0d127610f02ba94f012bb
Dmitriy Melnik <dmelnik@dengisrazy.ru>
2022-09-19 10:48:02 +0300

    test

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,37 @@
 const http = require('node:http');
+const {Client} = require("pg");
+const {DBConfig} = require("./configuration");
+
 
 // SERVER -----------------------------------------
 const port = 8080;
 
-const server = http.createServer((req, res) => {
-  console.log(req.url)
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+const apiHandler = async () => {
+  const client = new Client(DBConfig);
+  client.connect()
+
+  const res = await client
+     .query(`select * from users limit 5000`)
+
+  client.end()
+  return res.rows
+}
+
+const server = http.createServer(async (req, res) => {
+  const {url, method} = req
+  if (url === "/api" && method === "GET") {
+    const users = await apiHandler()
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({message: users}));
+    return
+  }
+  if (url === "/" && method === "GET") {
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({message: "Hello, World!\n"}));
+    return;
+  }
+  res.writeHead(404, {"Content-Type": "application/json"});
+  res.end(JSON.stringify({message: "Route not found"}));
 });
 
 server.listen(port, () => {
